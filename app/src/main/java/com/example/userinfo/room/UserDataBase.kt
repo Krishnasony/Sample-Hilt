4f06bf8c213d9d2ecfd916773cf74165c31bd6e3
Krishnasony <krishnasony97@gmail.com>
2020-06-20 23:40:45 +0530

    Add coroutine dependency

--- app/src/main/java/com/example/userinfo/room/UserDataBase.kt
+++ app/src/main/java/com/example/userinfo/room/UserDataBase.kt
@@ -1,4 +1,34 @@
 package com.example.userinfo.room
 
-class UserDataBase {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.userinfo.room.dao.UserDao
+import com.example.userinfo.room.entity.User
+
+@Database(entities = [User::class], version = 0)
+abstract class UserDataBase : RoomDatabase() {
+
+    abstract val userDao: UserDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: UserDataBase? = null
+        fun getInstance(context: Context): UserDataBase {
+            synchronized(this) {
+                var instance = INSTANCE
+                if (instance == null) {
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        UserDataBase::class.java,
+                        "user_data_database"
+                    ).build()
+                }
+                return instance
+            }
+        }
+
+    }
+
 }
