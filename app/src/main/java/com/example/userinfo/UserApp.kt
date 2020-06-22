c2e174b50a72384adb55ac99f6afeb550ccada03
Krishnasony <krishnasony97@gmail.com>
2020-06-22 20:44:01 +0530

    Setup stetho for database inspection

--- app/src/main/java/com/example/userinfo/UserApp.kt
+++ app/src/main/java/com/example/userinfo/UserApp.kt
@@ -1,7 +1,13 @@
 package com.example.userinfo
 
 import android.app.Application
+import com.facebook.stetho.Stetho
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class UserApp : Application()
+class UserApp : Application(){
+    override fun onCreate() {
+        super.onCreate()
+        Stetho.initializeWithDefaults(this)
+    }
+}
