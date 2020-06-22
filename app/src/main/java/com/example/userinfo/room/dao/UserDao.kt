019bdaac0bc31d9b7a366bd593e9548bf7c6cce1
Krishnasony <krishnasony97@gmail.com>
2020-06-22 21:40:22 +0530

    * Add user to database successfully * Get user from database successfully * Remove update user function

--- app/src/main/java/com/example/userinfo/room/dao/UserDao.kt
+++ app/src/main/java/com/example/userinfo/room/dao/UserDao.kt
@@ -1,7 +1,9 @@
 package com.example.userinfo.room.dao
 
-import androidx.lifecycle.LiveData
-import androidx.room.*
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
 import com.example.userinfo.room.entity.User
 
 @Dao
