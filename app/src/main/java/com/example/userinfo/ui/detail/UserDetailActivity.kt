138da8a93a94658f41c116958091c0ee47e54138
Krishnasony <krishnasony97@gmail.com>
2020-06-22 22:29:58 +0530

    Setup DataBinding for User detail UI

--- app/src/main/java/com/example/userinfo/ui/detail/UserDetailActivity.kt
+++ app/src/main/java/com/example/userinfo/ui/detail/UserDetailActivity.kt
@@ -5,11 +5,14 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import com.example.userinfo.R
+import com.example.userinfo.databinding.ActivityUserDetailBinding
 import com.example.userinfo.utils.Status
 import com.example.userinfo.utils.fullScreenView
-import com.example.userinfo.utils.showToast
+import com.example.userinfo.utils.gone
+import com.example.userinfo.utils.visible
 import com.example.userinfo.viewmodel.UserViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.activity_user_detail.*
@@ -17,30 +20,28 @@ import kotlinx.android.synthetic.main.activity_user_detail.*
 @AndroidEntryPoint
 class UserDetailActivity : AppCompatActivity() {
 
+    private lateinit var dataBinding: ActivityUserDetailBinding
     private val userViewModel: UserViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_user_detail)
+        dataBinding = DataBindingUtil.setContentView(this, R.layout.activity_user_detail)
 
         fullScreenView(R.color.colorAccent)
         btn_back.setOnClickListener {
             onBackPressed()
         }
 
-        userViewModel.user.observe(this, Observer {
-            when (it.status) {
-                Status.LOADING -> {
-                    showToast("loading")
-                }
+        userViewModel.user.observe(this, Observer { resource ->
+            when (resource.status) {
+                Status.LOADING -> progressBar.visible()
                 Status.SUCCESS -> {
-                    it.data?.let {
-                        showToast("User: $it")
+                    resource.data?.let {
+                        dataBinding.user = it
+                        progressBar.gone()
                     }
                 }
-                Status.ERROR -> {
-                    showToast("Error")
-                }
+                Status.ERROR -> progressBar.gone()
             }
         })
 
